a197bceb4b2bfdb82cf66532034a017fe0b66c35
Nick-Marx <93811081+Nick-Marx@users.noreply.github.com>
2021-12-11 19:30:06 -0600

    PROJECT5_COMPARISONS_TYPE_COERCION SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/js/main.js
@@ -0,0 +1,33 @@
+
+document.write(typeof 5 + "<br>");//writes the data type of 5 to page
+document.write(15 + "twenty" + "<br>");//writes 15twenty to page using type coercion
+document.write(2E310 + "<br>" + -3E310 + "<br>");//writes infinity and -infinity to page
+document.write(5>5);//compares if 5 is greater than 5
+document.write("<br>");//used to break line for readability on page
+document.write(10<11);//compares if 10 is less than 11
+document.write("<br>");
+console.log(5*3);//multiplies 5 by 3 and writes to console
+document.write(2==2);//compares if 2 is equal to 2
+document.write("<br>");
+document.write(4==2);//compares if 4 is equal to 2
+document.write("<br>");
+document.write(6===6);//compares if 6 is equal to 6 and shares the same data type
+document.write("<br>");
+document.write(7==="eight");//compares if 7 is equal to eight and shares the same data type
+document.write("<br>");
+document.write(9==="nine");//compares if 9 is equal to nine and shares the same data type
+document.write("<br>");
+document.write(33===23);//compares if 33 is equal to 23 and shares the same data type
+document.write("<br>");
+document.write(55 > 54 && 53 > 52);//uses compound boolean to determine if both comparisons are true
+document.write("<br>");
+document.write(55 > 54 && 53 < 52);//uses compound boolean to determine if both comparisons are true
+document.write("<br>");
+document.write(55 < 54 || 53 < 52);//uses compound boolean to determine if either of the comparisons are true
+document.write("<br>");
+document.write(55 > 54 || 53 < 52);//uses compound boolean to determine if either of the comparisons are true
+document.write("<br>");
+document.write(63 != 63);//determines if 63 is not equal to 63
+document.write("<br>");
+document.write(42 != 44);//determines if42 is not equal to 44
+document.write("<br>");
